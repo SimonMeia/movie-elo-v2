90f694a00cdcae39aba455ae002e218e0795a8a5
SimonMeia <simon@meia.dev>
2024-02-25 11:33:59 +0100

    fix(header): change logo sizing

--- resources/components/header.vue
+++ resources/components/header.vue
@@ -5,16 +5,14 @@ import Avatar from 'primevue/avatar'
 </script>
 
 <template>
-  <header class="sticky top-0 z-5 bg-primary-reverse border-bottom-1 h-4rem">
-    <div class="flex align-items-center justify-content-between py-2 p-container">
-      <Link href="/" :preserve-scroll="true" :preserve-state="true" aria-label="Homepage" class="h-3rem">
+  <header class="sticky top-0 z-5 bg-primary-reverse border-bottom-1">
+    <div class="flex align-items-center justify-content-between py-3 p-container">
+      <Link href="/" :preserve-scroll="true" :preserve-state="true" aria-label="Homepage">
         <MovieEloLogo></MovieEloLogo>
       </Link>
 
       <Avatar
         label="V"
-        class="mr-2"
-        size="normal"
         style="background-color: #ece9fc; color: #2a1261"
         shape="circle"
       />
