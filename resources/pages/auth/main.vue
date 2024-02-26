161d8d12a33b05e4be84dd5a0df82dc263810594
SimonMeia <simon@meia.dev>
2024-02-26 18:53:37 +0100

    fix(auth): overflows-hidden

--- resources/pages/auth/main.vue
+++ resources/pages/auth/main.vue
@@ -2,12 +2,11 @@
 import { router } from '@inertiajs/vue3'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
-import Password from 'primevue/password'
 import { computed } from 'vue'
 import { ref } from 'vue'
 
-const email = ref('')
-const password = ref('')
+const email = ref('simon@me.com')
+const password = ref('1234')
 const username = ref('')
 const firstName = ref('')
 const lastName = ref('')
@@ -22,6 +21,9 @@ const changeFormLabel = computed(() =>
 const changeFormButtonLabel = computed(() =>
   isLoginFormDisplayed.value ? "S'inscrire" : 'Se connecter'
 )
+
+const coverHeight = computed(() => (isLoginFormDisplayed.value ? '30%' : '15%'))
+const formHeight = computed(() => (isLoginFormDisplayed.value ? '70%' : '85%'))
 
 function submit() {
   const destination = isLoginFormDisplayed.value ? 'login' : 'register'
@@ -45,38 +47,47 @@ function submit() {
 
 <template>
   <div class="flex flex-column lg:flex-row h-screen">
-    <div class="w-full lg:w-6 lg:h-full" style="height: 30%;">
+    <div class="w-full lg:w-6 lg:h-full overflow-hidden" :style="{ height: coverHeight }">
       <img
         src="https://fr.web.img3.acsta.net/pictures/22/05/16/16/32/4176595.jpg"
         alt="hero"
-        class="h-full w-full"
+        class="w-full h-full"
         style="object-fit: cover"
       />
     </div>
-    <div class="w-full lg:w-6 lg:h-full flex flex-column justify-content-center px-8" style="height: 70%;">
-      <h1>Welcome back</h1>
-      <form @submit.prevent="submit" class="flex flex-column gap-4 w-full">
-        <div v-if="!isLoginFormDisplayed">
-          <label for="">Prénom</label><br />
-          <InputText v-model="firstName" class="w-full" />
+    <div
+      class="w-full lg:w-6 lg:h-full flex flex-column px-4 md:px-8"
+      :style="{ height: formHeight }"
+    >
+      <form @submit.prevent="submit" class="flex flex-column w-full flex-grow-1 my-3 md:my0">
+        <h1
+          class="text-2xl md:text-4xl font-bold pt-1 mb-0 md:flex-grow-1 flex flex-column justify-content-end"
+        >
+          Welcome back
+        </h1>
+        <div class="flex-grow-1 flex justify-content-center flex-column gap-3 md:gap-4">
+          <div v-if="!isLoginFormDisplayed">
+            <label for="">Prénom</label><br />
+            <InputText v-model="firstName" class="w-full" />
+          </div>
+          <div v-if="!isLoginFormDisplayed">
+            <label for="">Nom</label><br />
+            <InputText v-model="lastName" class="w-full" />
+          </div>
+          <div v-if="!isLoginFormDisplayed">
+            <label for="">Nom d'utilisateur</label><br />
+            <InputText v-model="username" class="w-full" />
+          </div>
+          <div>
+            <label for="">Email</label><br />
+            <InputText v-model="email" class="w-full" />
+          </div>
+          <div>
+            <label for="">Password</label><br />
+            <InputText type="password" v-model="password" class="w-full" />
+          </div>
         </div>
-        <div v-if="!isLoginFormDisplayed">
-          <label for="">Nom</label><br />
-          <InputText v-model="lastName" class="w-full" />
-        </div>
-        <div v-if="!isLoginFormDisplayed">
-          <label for="">Nom d'utilisateur</label><br />
-          <InputText v-model="username" class="w-full" />
-        </div>
-        <div>
-          <label for="">Email</label><br />
-          <InputText v-model="email" class="w-full" />
-        </div>
-        <div>
-          <label for="">Password</label><br />
-          <InputText type="password" v-model="password" class="w-full" />
-        </div>
-        <div>
+        <div class="flex flex-column align-items-center md:flex-grow-1">
           <Button type="submit" class="w-full" :label="sumbitButtonLabel"></Button>
           <div class="flex flex-row gap-2 align-items-center mt-2">
             <span>{{ changeFormLabel }}</span>
