b5e5b6521db352744a7e700c73853c4622bd737e
SimonMeia <simon@meia.dev>
2024-02-26 18:08:58 +0100

    feat(auth): login screen style

--- resources/pages/auth/main.vue
+++ resources/pages/auth/main.vue
@@ -43,41 +43,51 @@ function submit() {
 </script>
 
 <template>
-  <div class="p-container">
-    <h1>Auth</h1>
-    <form @submit.prevent="submit" class="flex flex-column gap-2 w-full md:w-8 lg:w-6">
-      <div v-if="!isLoginFormDisplayed">
-        <label for="">Prénom</label><br />
-        <InputText v-model="firstName" class="w-full" />
-      </div>
-      <div v-if="!isLoginFormDisplayed">
-        <label for="">Nom</label><br />
-        <InputText v-model="lastName" class="w-full" />
-      </div>
-      <div v-if="!isLoginFormDisplayed">
-        <label for="">Nom d'utilisateur</label><br />
-        <InputText v-model="username" class="w-full" />
-      </div>
-      <div>
-        <label for="">Email</label><br />
-        <InputText v-model="email" class="w-full" />
-      </div>
-      <div>
-        <label for="">Password</label><br />
-        <InputText type="password" v-model="password" class="w-full" />
-      </div>
-      <div>
-        <Button type="submit"  class="w-full" :label="sumbitButtonLabel"></Button>
-        <div class="flex flex-row gap-2 align-items-center mt-2">
-          <span>{{ changeFormLabel }}</span>
-          <Button
-            link
-            :label="changeFormButtonLabel"
-            @click="isLoginFormDisplayed = !isLoginFormDisplayed"
-          ></Button>
+  <div class="flex flex-column lg:flex-row h-screen">
+    <div class="w-full lg:w-6 lg:h-full" style="height: 30%;">
+      <img
+        src="https://fr.web.img3.acsta.net/pictures/22/05/16/16/32/4176595.jpg"
+        alt="hero"
+        class="h-full w-full"
+        style="object-fit: cover"
+      />
+    </div>
+    <div class="w-full lg:w-6 lg:h-full flex flex-column justify-content-center px-8" style="height: 70%;">
+      <h1>Welcome back</h1>
+      <form @submit.prevent="submit" class="flex flex-column gap-4 w-full">
+        <div v-if="!isLoginFormDisplayed">
+          <label for="">Prénom</label><br />
+          <InputText v-model="firstName" class="w-full" />
         </div>
-      </div>
-    </form>
+        <div v-if="!isLoginFormDisplayed">
+          <label for="">Nom</label><br />
+          <InputText v-model="lastName" class="w-full" />
+        </div>
+        <div v-if="!isLoginFormDisplayed">
+          <label for="">Nom d'utilisateur</label><br />
+          <InputText v-model="username" class="w-full" />
+        </div>
+        <div>
+          <label for="">Email</label><br />
+          <InputText v-model="email" class="w-full" />
+        </div>
+        <div>
+          <label for="">Password</label><br />
+          <InputText type="password" v-model="password" class="w-full" />
+        </div>
+        <div>
+          <Button type="submit" class="w-full" :label="sumbitButtonLabel"></Button>
+          <div class="flex flex-row gap-2 align-items-center mt-2">
+            <span>{{ changeFormLabel }}</span>
+            <Button
+              link
+              :label="changeFormButtonLabel"
+              @click="isLoginFormDisplayed = !isLoginFormDisplayed"
+            ></Button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
