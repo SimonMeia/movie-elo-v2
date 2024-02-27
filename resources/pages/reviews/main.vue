540285eafe2f15d8c1400195f757fc195e0fb991
SimonMeia <simon@meia.dev>
2024-02-27 07:19:37 +0100

    feat(reviews): change ul to datatable

--- resources/pages/reviews/main.vue
+++ resources/pages/reviews/main.vue
@@ -1,22 +1,47 @@
 <script setup lang="ts">
 import type { ReviewsResponse } from '@/types'
 import { watchEffect } from 'vue'
+import Layout from '@/layouts/default.vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { computed } from 'vue'
+import type { Ref } from 'vue'
+import { ref } from 'vue'
+import { router } from '@inertiajs/vue3'
+
+interface ReviewListItem {
+  reviewId: number
+  title: string
+}
 
 const props = defineProps<ReviewsResponse>()
+const selectedReview: Ref<ReviewListItem | null> = ref(null)
 
-watchEffect(() => {
-  console.log(props)
-})
+const reviewsList = computed(() =>
+  props.reviews.map((review): ReviewListItem => {
+    return { reviewId: review.review.id, title: review.movie.title }
+  })
+)
+
+function onRowSelect(event) {
+  console.log(event.data.title)
+  router.get('/reviews/' + event.data.reviewId)
+}
 </script>
 
 <template>
-  <div class="p-container">
-    <ul>
-      <li v-for="review of props.reviews">
-        {{ review.movie.title }}
-      </li>
-    </ul>
-  </div>
+  <Layout>
+    <div class="p-container">
+      <DataTable
+        :value="reviewsList"
+        v-model:selection="selectedReview"
+        selectionMode="single"
+        @rowSelect="onRowSelect"
+      >
+        <Column field="title"></Column>
+      </DataTable>
+    </div>
+  </Layout>
 </template>
 
 <style scoped></style>
