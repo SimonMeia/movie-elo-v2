9853155dc6f9c0c037d190e832c9dc5da0192385
SimonMeia <simon@meia.dev>
2024-02-28 23:07:18 +0100

    feat(header): add menu

--- resources/components/header.vue
+++ resources/components/header.vue
@@ -2,6 +2,8 @@
 import { Link, router } from '@inertiajs/vue3'
 import MovieEloLogo from '@/assets/icons/movieelo_logo.vue'
 import Avatar from 'primevue/avatar'
+import Button from 'primevue/button'
+import Divider from 'primevue/divider';
 </script>
 
 <template>
@@ -10,6 +12,12 @@ import Avatar from 'primevue/avatar'
       <Link href="/" :preserve-scroll="true" :preserve-state="true" aria-label="Homepage">
         <MovieEloLogo></MovieEloLogo>
       </Link>
+
+      <div class="grow flex justify-end mr-12">
+        <Button label="Ajouter une review" link @click="router.get('/review-form')" />
+        <Button label="Mes reviews" link @click="router.get('/reviews')" />
+        <Button label="Elo ranking" link disabled @click="router.get('/elo-ranking')" />
+      </div>
 
       <Avatar
         label="V"
