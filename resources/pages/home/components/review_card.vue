6d8a12e4eeb7931328a0cfebcad00d6fe0d4ba80
SimonMeia <simon@meia.dev>
2024-02-28 23:25:45 +0100

    feat(home cards): add hover effect

--- resources/pages/home/components/review_card.vue
+++ resources/pages/home/components/review_card.vue
@@ -13,7 +13,10 @@ const tagColor = totalGrade > 80 ? 'success' : totalGrade <= 50 ? 'danger' : 'wa
 </script>
 
 <template>
-  <div class="relative border border-gray-200 rounded-lg shadow flex flex-col">
+  <div
+    class="card relative border border-gray-200 rounded-lg shadow flex flex-col"
+    @click="router.get(`/reviews/${review.review.id}`)"
+  >
     <Tag :value="totalGrade" :severity="tagColor" class="absolute top-2 right-2" />
     <img
       :src="`https://image.tmdb.org/t/p/original${review.movie.posterPath}`"
@@ -21,13 +24,37 @@ const tagColor = totalGrade > 80 ? 'success' : totalGrade <= 50 ? 'danger' : 'wa
       class="object-cover w-full rounded-t-lg"
     />
     <div class="flex flex-col justify-between p-5 h-full">
-      <h2 class="mb-2 font-bold text-l">
-        {{ review.movie.title }}
-      </h2>
-
+      <span>
+        <h2 class="mb-2 inline link-underline link-underline-black">
+          {{ review.movie.title }}
+        </h2>
+      </span>
       <Button label="Voir la review" link @click="router.get(`/reviews/${review.review.id}`)" />
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.link-underline {
+  border-bottom-width: 0;
+  background-image: linear-gradient(transparent, transparent), linear-gradient(#fff, #fff);
+  background-size: 0 3px;
+  background-position: 0 100%;
+  background-repeat: no-repeat;
+  transition: background-size 0.5s ease-in-out;
+}
+
+.link-underline-black {
+  background-image: linear-gradient(transparent, transparent), linear-gradient(#c21313, #c21313);
+}
+
+.link-underline:hover {
+  background-size: 100% 3px;
+  background-position: 0 100%;
+}
+
+.card:hover .link-underline {
+  background-size: 100% 3px;
+  background-position: 0 100%;
+}
+</style>
