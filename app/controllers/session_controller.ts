74816a4bf1f7c7cd4128b4f8fb5d0b207ce00128
SimonMeia <simon@meia.dev>
2024-03-27 19:54:37 +0100

    refactor: change redirect

--- app/controllers/session_controller.ts
+++ app/controllers/session_controller.ts
@@ -29,7 +29,7 @@ export default class SessionController {
 
     await auth.use('web').login(user)
 
-    return response.redirect().toRoute('/')
+    return response.redirect().toRoute('/grade-categories')
   }
 
   @inject()
