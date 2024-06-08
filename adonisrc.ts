2fd916b835b991a6dd20c261eec80473b9314737
SimonMeia <92148227+SimonMeia@users.noreply.github.com>
2024-06-08 15:21:30 +0200

    deps: update packages

--- adonisrc.ts
+++ adonisrc.ts
@@ -83,4 +83,10 @@ export default defineConfig({
       reloadServer: false,
     },
   ],
+
+
+  assetsBundler: false,
+  hooks: {
+    onBuildStarting: [() => import('@adonisjs/vite/build_hook')],
+  },
 })
