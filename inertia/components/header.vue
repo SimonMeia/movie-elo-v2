6d87028d3b8d9c5a7e5c3e9d019dbb3c4e941290
SimonMeia <92148227+SimonMeia@users.noreply.github.com>
2025-01-06 10:53:38 +0100

    ui: fix mobile menu

--- inertia/components/header.vue
+++ inertia/components/header.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Link, router } from '@inertiajs/vue3'
 import MovieEloLogo from '@/assets/icons/movieelo_logo.vue'
+import MovieEloLogoSmall from '@/assets/icons/movieelo_logo_small.vue'
 import Avatar from 'primevue/avatar'
 import Button from 'primevue/button'
 import Menubar from 'primevue/menubar'
@@ -35,14 +36,13 @@ const items = ref([
   <header class="sticky top-0 z-50 border-b border-accent">
     <Menubar :model="items" :pt="{ root: '!bg-white' }">
       <template #start>
-        <Link
-          href="/"
-          :preserve-scroll="true"
-          :preserve-state="true"
-          aria-label="Homepage"
-          class="mr-4"
-        >
-          <MovieEloLogo></MovieEloLogo>
+        <Link href="/" :preserve-scroll="true" :preserve-state="true" aria-label="Homepage">
+          <div class="hidden mr-4 md:block">
+            <MovieEloLogo></MovieEloLogo>
+          </div>
+          <div class="block h-8 md:hidden">
+            <MovieEloLogoSmall></MovieEloLogoSmall>
+          </div>
         </Link>
       </template>
       <template #item="{ item, props, hasSubmenu, root }">
