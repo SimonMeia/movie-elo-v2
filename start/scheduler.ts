ed59c352dbe6aa9b4fa02925aa60a12217358fc4
SimonMeia <92148227+SimonMeia@users.noreply.github.com>
2025-01-10 14:54:45 +0100

    fix: remove test logs

--- start/scheduler.ts
+++ start/scheduler.ts
@@ -1,9 +1,5 @@
 import scheduler from 'adonisjs-scheduler/services/main'
-import logger from '@adonisjs/core/services/logger'
 
 import ResetDemoUser from '../commands/reset_demo_user.js'
 
 scheduler.command(ResetDemoUser).daily()
-
-scheduler.call(() => console.log('console.log')).everyMinute()
-scheduler.call(() => logger.info('logger.info')).everyMinute()
