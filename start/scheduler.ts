e544fab0e27a0e0cde111b3ae6a20a4e02b58cc5
SimonMeia <92148227+SimonMeia@users.noreply.github.com>
2025-01-10 14:39:56 +0100

    ci: logs

--- start/scheduler.ts
+++ start/scheduler.ts
@@ -2,3 +2,5 @@ import scheduler from 'adonisjs-scheduler/services/main'
 import ResetDemoUser from '../commands/reset_demo_user.js'
 
 scheduler.command(ResetDemoUser).daily()
+
+scheduler.call(() => console.log('Test')).everyMinute()
