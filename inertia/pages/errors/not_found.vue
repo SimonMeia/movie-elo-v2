2b144463946235fc7510df69b091fbada0dde931
SimonMeia <92148227+SimonMeia@users.noreply.github.com>
2025-01-11 15:15:05 +0100

    feat: better error pages

--- inertia/pages/errors/not_found.vue
+++ inertia/pages/errors/not_found.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import Layout from '@/layouts/default.vue'
 import Button from 'primevue/button'
 import Card from 'primevue/card'
+import { router } from '@inertiajs/vue3'
 </script>
 <template>
-  <Layout>
-    <div class="container flex justify-center mt-8">
-      <Card class="w-fit p-8 text-center">
-        <template #content>
-          <div class="space-y-4">
-            <h1 class="text-9xl text-accent">404</h1>
-            <h2 class="title">Page not found</h2>
-            <div>La page souhaitée n'est pas disponible</div>
-            <Button label="Retour à la homepage" />
-          </div>
-        </template>
-      </Card>
-    </div>
-  </Layout>
+  <!--  <Layout>-->
+  <div class="container flex justify-center mt-8">
+    <Card class="w-fit p-8 text-center">
+      <template #content>
+        <div class="space-y-4">
+          <h1 class="text-9xl text-accent">404</h1>
+          <h2 class="title">Page not found</h2>
+          <div>La page souhaitée n'est pas disponible</div>
+          <Button label="Retour à la homepage" @click="router.get('/')" />
+        </div>
+      </template>
+    </Card>
+  </div>
+  <!--  </Layout>-->
 </template>
--- inertia/pages/errors/server_error.vue
+++ inertia/pages/errors/server_error.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import Layout from '@/layouts/default.vue'
 import Button from 'primevue/button'
 import Card from 'primevue/card'
+import { router } from '@inertiajs/vue3'
 
 defineProps<{ errorCode: number }>()
 </script>
 <template>
-  <Layout>
-    <div class="container flex justify-center mt-8">
-      <Card class="w-fit p-8 text-center">
-        <template #content>
-          <div class="space-y-4">
-            <h1 class="text-9xl text-accent">{{ errorCode }}</h1>
-            <h2 class="title">Server error</h2>
-            <div>La page souhaitée n'est pas disponible</div>
-            <Button label="Retour à la homepage" />
-          </div>
-        </template>
-      </Card>
-    </div>
-  </Layout>
+  <div class="container flex justify-center mt-8">
+    <Card class="w-fit p-8 text-center">
+      <template #content>
+        <div class="space-y-4">
+          <h1 class="text-9xl text-accent">{{ errorCode }}</h1>
+          <h2 class="title">Server error</h2>
+          <div>La page souhaitée n'est pas disponible</div>
+          <Button label="Retour à la homepage" @click="router.get('/')" />
+        </div>
+      </template>
+    </Card>
+  </div>
 </template>
