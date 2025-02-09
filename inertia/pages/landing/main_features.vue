9fb2d0a66f66256f10a2566fde7f08def9f16d8f
SimonMeia <simon@meia.dev>
2025-02-09 14:39:56 +0100

    wip: landing page

--- inertia/pages/landing/main_features.vue
+++ inertia/pages/landing/main_features.vue
@@ -1,7 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { FormGradeType } from '@/app/types'
+
+const dbGradeTypes: FormGradeType[] = [
+  {
+    id: 1,
+    name: 'Acteurs',
+    maxGrade: 10,
+    grades: [
+      { id: 1, grade: 1, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+      { id: 2, grade: 2, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+      { id: 3, grade: 3, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+      { id: 4, grade: 4, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Acteurs',
+    maxGrade: 10,
+    grades: [
+      { id: 1, grade: 1, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+      { id: 2, grade: 2, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Acteurs',
+    maxGrade: 10,
+    grades: [
+      { id: 1, grade: 1, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+      { id: 2, grade: 2, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+      { id: 3, grade: 3, description: 'Le meilleur acteur', movie: 'Le meilleur acteur' },
+    ],
+  },
+]
+</script>
 
 <template>
-  <div class="grid gap-4 lg:grid-cols-2" id="main-features">
+  <div class="grid items-center gap-4 lg:grid-cols-2" id="main-features">
     <div class="flex flex-col gap-4">
       <h2 class="text-3xl font-titles">Qui a dit que 5 étoiles suffisaient ?</h2>
       <p>
@@ -13,6 +48,22 @@
         <li>Choisissez une note maximale pour chaque catégorie</li>
         <li>Décrivez chaque notes associer des émotions aux chiffres</li>
       </ul>
+    </div>
+
+    <div>
+      <Accordion>
+        <AccordionPanel v-for="gradeType in dbGradeTypes" :key="gradeType.id" :value="gradeType.id">
+          <AccordionHeader>{{ gradeType.name }}</AccordionHeader>
+          <AccordionContent>
+            <div class="grid grid-cols-1 gap-2">
+              <div v-for="grade in gradeType.grades" :key="grade.id">
+                <h4 class="text-accent">{{ grade.grade }} - {{ grade.description }}</h4>
+                <p>{{ grade.movie }}</p>
+              </div>
+            </div>
+          </AccordionContent>
+        </AccordionPanel>
+      </Accordion>
     </div>
   </div>
 
@@ -32,6 +83,8 @@
         </li>
       </ul>
     </div>
+
+    <div></div>
   </div>
 
   <div class="grid gap-4 lg:grid-cols-2">
@@ -46,6 +99,8 @@
         <li>Comparison simplifée sans avoir a aller dans le tableau</li>
       </ul>
     </div>
+
+    <div></div>
   </div>
 </template>
 
